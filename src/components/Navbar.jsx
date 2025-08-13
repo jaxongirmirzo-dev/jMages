095c373d081c71cbc7cbd668f4f256fc666290d8
Jahongirmirzo <devmirzo19@gmail.com>
2025-08-13 08:26:43 +0430

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,9 +8,18 @@ function Navbar() {
   console.log(user);
   return (
     <div className="flex">
-      <h1>Navbar</h1>
-      <img className="rounded-full" src={user?.photoURL} alt="" width={200} />
-      <h3>{user?.displayName}</h3>
+      <h1 className="text-5xl font-mono text-shadow-accent mr-auto [text-shadow:1px_10px_5px_rgba(0,0,0,0.3)]">
+          Jimages
+        </h1>
+      <div className="flex items-center mr-auto">
+          <img
+            className="rounded-full bg-black/45 shadow-[0_4px_10px_black] hover:cursor-pointer mr-4"
+            src={user?.photoURL}
+            alt="user ava"
+            width={80}
+          />
+          <h3 className="text-2xl font-sans underline">{user?.displayName}</h3>
+        </div>
       {!isPending && (
         <button onClick={logOut} className="btn btn-primary">
           LogOut
